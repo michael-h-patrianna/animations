0b59ff87030b27b7c74b280dfbf1851c09b518df
michael haufschild <michael.h@patrianna.com>
2025-09-21 20:55:59 +0200

    remove unused fonts

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { preloadImages } from '@/lib/preload'
+import { CRITICAL_ICON_IMAGES } from '@/lib/preload-manifest'
+
+// Preload critical icon animation images ASAP at startup
+preloadImages(CRITICAL_ICON_IMAGES)
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
