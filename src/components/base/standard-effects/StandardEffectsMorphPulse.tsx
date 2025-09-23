c22c8546dee1efb83ef89e3a3f71916d3814a15d
michael haufschild <michael.h@patrianna.com>
2025-09-23 21:49:01 +0200

    new indicators and pulses

---
+++ src/components/base/standard-effects/StandardEffectsMorphPulse.tsx
@@ -0,0 +1,9 @@
+import './StandardEffectsMorphPulse.css'
+
+export function StandardEffectsMorphPulse() {
+  return (
+    <div className="morph-pulse-wrapper">
+      <div className="morph-pulse" role="img" aria-label="Morph pulse blob" />
+    </div>
+  )
+}
